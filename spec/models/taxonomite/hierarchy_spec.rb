b82afbbb08769453fb5ceae9db103c09e4d341c5
hgillesp <hsgvmd@gmail.com>
2015-08-13 16:38:29 -0700

    fix formatting, orgainzation

--- spec/models/taxonomite/hierarchy_spec.rb
+++ spec/models/taxonomite/hierarchy_spec.rb
@@ -3,32 +3,54 @@ require 'models/taxonomite/zoology'
 
 module Taxonomite
     RSpec.describe Tree, type: :model do
+        describe 'enforce hierarchy' do
+            context 'without Taxonomy object' do
+                before(:each) do
+                    @family = FactoryGirl.build(:taxonomite_family)
+                    @genus = FactoryGirl.build(:taxonomite_genus)
+                    @species = FactoryGirl.build(:taxonomite_species)
+                end
 
-      context 'sample hierarchy enforcement' do
-          before(:each) do
-              @family = FactoryGirl.build(:taxonomite_family)
-              @genus = FactoryGirl.build(:taxonomite_genus)
-              @species = FactoryGirl.build(:taxonomite_species)
-          end
+                it 'to allow species in genus when no taxonomy present' do
+                    expect { @genus.children << @species }.not_to raise_error
+                end
 
-          it 'to allow species in genus' do
-              expect { @genus.children << @species }.not_to raise_error
-          end
+                it 'to allow genus in family when no taxonomy present' do
+                    expect { @family.children << @genus }.not_to raise_error
+                end
 
-          it 'to allow genus in family' do
-              expect { @family.children << @genus }.not_to raise_error
-          end
+                it 'to allow genus in species when no taxonomy present' do
+                    expect { @species.children << @genus }.not_to raise_error
+                end
 
-          it 'not to allow genus in species' do
-              expect { @species.children << @genus }.to raise_error
-          end
+                it 'to allow species in family when no taxonomy present' do
+                    expect { @family.children << @species }.not_to raise_error
+                end
+            end
 
-          it 'not to allow species in family' do
-              expect { @family.children << @species }.to raise_error
-          end
+            context 'with Taxonomy object' do
+                before(:each) do
+                    @family = FactoryGirl.build(:taxonomite_family)
+                    @genus = FactoryGirl.build(:taxonomite_genus)
+                    @species = FactoryGirl.build(:taxonomite_species)
+                end
 
+                it 'to allow species in genus when no taxonomy present' do
+                    expect { @genus.children << @species }.not_to raise_error
+                end
+
+                it 'to allow genus in family when no taxonomy present' do
+                    expect { @family.children << @genus }.not_to raise_error
+                end
+
+                it 'to allow genus in species when no taxonomy present' do
+                    expect { @species.children << @genus }.not_to raise_error
+                end
+
+                it 'to allow species in family when no taxonomy present' do
+                    expect { @family.children << @species }.not_to raise_error
+                end
+            end
       end
-
     end
-
 end
